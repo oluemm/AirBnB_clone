f85960db89a87150ff8db6ebf85c2a3fb8e582fe
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-23 14:21:15 +0100

    filestorage class: created reload() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,9 @@ class FileStorage that serializes instances to a JSON file
 and deserializes JSON file to instances
 """
 import json
+from models.base_model import BaseModel
+
+class_dict = {"BaseModel": BaseModel}
 
 
 class FileStorage():
@@ -22,6 +25,10 @@ class FileStorage():
     - all(self): returns the dictionary __objects
     - new(self, obj): sets in __objects the obj with key <obj class name>.id
     - save(self): serializes __objects to the JSON file (path: __file_path)
+    - reload(self): deserializes the JSON file to __objects
+    (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+    If the file doesn’t exist, no exception would be raised)
+
     """
     # private class attributes
     __file_path = "file.json"
@@ -49,3 +56,18 @@ class FileStorage():
             for k, v in self.__objects.items():
                 dict_storage[k] = v.to_dict()
             json.dump(dict_storage, f)
+
+    def reload(self):
+        """
+        Deserializes existing JSON file to __objects
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                # load the json file as instance object
+                instance = json.load(f)
+            for key in instance:
+                self.__objects[key] = (
+                    class_dict[instance[key]["__class__"]](**instance[key])
+                    )
+        except Exception:
+            pass
