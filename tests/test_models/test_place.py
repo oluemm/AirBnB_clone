e6b2541ccca13637015524b383c1062077901a7a
Yofty <106748943+Yofty@users.noreply.github.com>
2022-11-26 11:54:42 -0800

    task 9

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""test module for place class"""
+import unittest
+
+from models.base_model import BaseModel
+from models.place import Place
+
+class TestPlace(unittest.TestCase):
+    """test case for place class"""
+    def setUp(self):
+        self.place = Place()
+        self.attr_list = ["name", "user_id", "city_id", "description",
+                "number_bathrooms", "max_guest", "number_rooms",
+                 "price_by_night", "latitude", "longitude", "amenity_ids"]
+
+    def test_place_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.place), BaseModel))
+
+    def test_attrs(self):
+        for attr in self.attr_list:
+            self.assertTrue(hasattr(Place, attr))
+
+    def test_class_attrs(self):
+        self.assertIs(type(self.place.name), str)
+        self.assertIs(type(self.place.city_id), str)
+        self.assertIs(type(self.place.user_id), str)
+        self.assertIs(type(self.place.description), str)
+        self.assertIs(type(self.place.number_bathrooms), int)
+        self.assertIs(type(self.place.max_guest), int)
+        self.assertIs(type(self.place.number_rooms), int)
+        self.assertIs(type(self.place.price_by_night), int)
+        self.assertIs(type(self.place.latitude), float)
+        self.assertIs(type(self.place.longitude), float)
+        self.assertIs(type(self.place.amenity_ids), list)
+
+        for attr in se;f.attr_list:
+            self.assertFalse(bool(getattr(self.place, attr)))
