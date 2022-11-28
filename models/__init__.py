b6f3b93e81e94aae1d39889cde211a9359a655f7
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-28 21:08:04 +0100

    mods

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.engine.file_storage import class_dict
-
 
 # initialize an instance of FileStorage called storage
 storage = FileStorage()
